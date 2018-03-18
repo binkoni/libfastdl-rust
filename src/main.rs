a4f6a8927dad67fc490ec9c60ef4973f32a54ef1
Byeonggon Lee <gonny952@gmail.com>
2018-03-18 20:55:56 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+extern crate libloading as lib;
+use std::collections::HashMap;
+use std::mem::transmute;
+fn main() {
+    let lib = lib::Library::new("libembed.so").unwrap();
+    let mut fn_map: HashMap<&str, lib::Symbol<*const ()>> = HashMap::new();
+    unsafe {
+        let func: lib::Symbol<*const ()> = lib.get(b"test").unwrap();
+        fn_map.insert("test", func);
+        let func = fn_map.get("test").unwrap();
+        let func = transmute::<&lib::Symbol<*const()>, &lib::Symbol<unsafe extern fn()>>(func);
+        func();
+    }
+    println!("This is main function");
+}
