f08ae3a206caea14faecc26a6f9d73c0efb8ad39
Byeonggon Lee <gonny952@gmail.com>
2018-03-27 12:37:24 +0900

    dylibexample -> dltest

--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 #[test]
 fn test_success() {
-    let mut lib = super::Library::new("libdylibexample.so").unwrap();
+    let mut lib = super::Library::new("libdltest.so").unwrap();
     unsafe {
         let test_fn = lib.get::<unsafe extern fn(i32) -> i32>("test").unwrap();
         assert!(test_fn(100) == 200);
@@ -21,7 +21,7 @@ fn test_file_not_found() {
 #[test]
 #[should_panic]
 fn test_symbol_not_found() {
-    let mut lib = super::Library::new("libdylibexample.so").unwrap();
+    let mut lib = super::Library::new("libdltest.so").unwrap();
     unsafe {
         let test_fn = lib.get::<unsafe extern fn(i32) -> i32>("whatever").unwrap();
         assert!(test_fn(100) == 200);
